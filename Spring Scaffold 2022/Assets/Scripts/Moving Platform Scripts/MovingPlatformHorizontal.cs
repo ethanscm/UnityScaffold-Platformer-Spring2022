bf94cfb2b3e366713b6ef31b366d3e56bcf02fa4
klluu <kevinluu228@gmail.com>
2022-05-11 18:50:14 -0700

    Created Moving Platforms
    will work on the switch another day

---
+++ Spring Scaffold 2022/Assets/Scripts/Moving Platform Scripts/MovingPlatformHorizontal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformHorizontal : MonoBehaviour
+{
+    private float speed = 2;
+    private int startingPoint = 0;
+    public Transform[] points;
+
+    private int i;
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+    }
+
+    
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+}
---
+++ Spring Scaffold 2022/Assets/Scripts/Moving Platform Scripts/StoppingPlatform.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoppingPlatform : MonoBehaviour
+{
+    private float speed = 2;
+    private int startingPoint = 0;
+    public Transform[] points;
+    private bool switchON = true;
+
+    private int i;
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+    }
+
+    
+    void Update()
+    {
+        if (switchON)
+        {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+            {
+                i++;
+                if (i == points.Length)
+                {
+                    i = 0;
+                }
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        }
+    }
+}
