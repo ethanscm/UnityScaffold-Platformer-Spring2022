d4d2128b2f87670c20d86d3ee822f96489e24a95
m-tsoi <mjtsoi@uci.edu>
2022-05-31 18:12:13 -0700

    woodpecker (incomplete)
    - added woodpecker object in "woodpecker" scene
    - works with woodpecker script, create new empty object called "WoodPecker" and attach the script to it
    - right now it moves left to right, will (hopefully) change that soon (maybe make it loop instead?)

---
+++ Spring Scaffold 2022/Assets/Scripts/Enemy Scripts/WoodPecker.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))] 
+
+public class WoodPecker : MonoBehaviour
+{
+    //reference to Waypoints
+    public List<Transform> points;
+    public int nextID = 0;
+    private int idChangeValue = 1;
+    public float speed = 2;
+
+
+	public void Reset()
+	{
+        Init();
+
+
+	}
+
+	public void Init()
+	{
+        //Make Box Collider Trigger
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        //Create Root Object
+        GameObject root = new GameObject(name + "_Root");
+        root.transform.position = transform.position;
+        transform.SetParent(root.transform);
+
+        GameObject waypoints = new GameObject("Waypoints");
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+
+
+
+        GameObject p1 = new GameObject("Point1");
+        p1.transform.SetParent(waypoints.transform);
+        p1.transform.position = Vector3.zero;
+
+        GameObject p2 = new GameObject("Point2");
+        p2.transform.SetParent(waypoints.transform);
+        p2.transform.position = Vector3.zero;
+
+
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+
+
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        MoveToNextPoint();
+    }
+
+
+    public void MoveToNextPoint()
+	{
+        Transform goalPoint = points[nextID];
+
+        if (goalPoint.transform.position.x > transform.position.x)
+        {
+            //transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-5, 5, 5);
+        }
+        else
+        {
+            //transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(5, 5, 5);
+
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed*Time.deltaTime );
+        if (Vector2.Distance(transform.position, goalPoint.position) < 1f)
+        {
+            if (nextID == points.Count - 1)
+            {
+                idChangeValue = -1;
+            }
+
+            if (nextID == 0)
+			{
+                idChangeValue = 1;
+			}
+
+            nextID += idChangeValue;
+        }
+    }
+
+    //functions to kill upon impact
+
+    //function to respawn
+
+
+}
